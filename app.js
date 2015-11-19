80665e8b336077dcc2e6887f644b257509be6354
Marco Pariente-Cohen <markypc3@gmail.com>
2015-11-19 14:27:45 -0500

    set static routes to serve root to /public. The index.html is rendering and displaying the expected hellow world and list items

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 let express = require('express');
 let app = express();
 let artistRoutes = require('./controllers/artists_controller')
+app.use(express.static('./public'));
 app.use('/artists', artistRoutes);
-
 
 let server = app.listen(3000,function() {
   let host = server.address().address;
